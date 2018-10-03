3f04cab1a6328f72f1b548a95bf5b487b788dd67
radenle <43663248+radenle@users.noreply.github.com>
2018-10-02 21:34:15 -0500

    Add files via upload

---
+++ chap 5.14.py
@@ -0,0 +1,9 @@
+triangle_char = input('Enter a character:\n')
+triangle_height = int(input('Enter triangle height:\n'))
+print('')
+i = 0
+while i < triangle_height :
+    i += 1
+    print ((triangle_char + ' ')*i)
+    #print (' ')
+#print ('* * * ')
